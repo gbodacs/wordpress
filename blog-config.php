bdc30d70e5ced5e2c2f6ccd125f3efe1b1e31204
Gabor Bodacs <gbodacs@gmail.com>
2020-08-16 21:46:21 +0200

    CSS magic

--- blog-config.php
+++ blog-config.php
@@ -1,5 +1,5 @@
 $output_str = '<table style="border-width:0px;padding:1px 0px;line-height:1.0;"><thead><tr style="border-width:0px;padding:0px 0px;"><th rowspan="2" width=42 height=42 style="text-align:center;vertical-align:middle;border-width:0px;padding:0px 0px;"><img src="https://relaxing-sleep.com/wp-content/uploads/2020/07/ann-3.png" alt="Ann Donazy"></th>
 		<th style="border-width:0px;padding:1px 5px;font-size:14px;">'.'Written by: '.astra_post_author().'</th>
-		</tr><tr>
+		</tr><tr class="trbg">
 		<td style="border-width:0px;padding:1px 5px;font-size:11px;">'.'Updated: '.astra_post_date().'</td>
 		</tr></thead></table>';
